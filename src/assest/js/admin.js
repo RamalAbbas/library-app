22a57613442b39b9ecab81b337a3e61be2f24953
Ramal Abbassov <ramalabbasov044@gmail.com>
2023-09-22 11:39:20 +0400

    add

--- src/assest/js/admin.js
+++
@@ -1,6 +0,0 @@
-let admin_join_btn = document.querySelector("#admin_join_btn")
-let admin_body = document.querySelector("#admin_body")
-
-admin_join_btn.addEventListener('click',function(){
-    admin_body.classList.toggle("admin")
-})
