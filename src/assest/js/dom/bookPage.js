b6c763a5b239628a7b3de2740334607622c2d633
Dunyanur Seferov <127347425+safarov211@users.noreply.github.com>
2023-10-03 11:00:11 +0400

    add

--- src/assest/js/dom/bookPage.js
+++ src/assest/js/dom/bookPage.js
@@ -2,7 +2,8 @@ let comment_input = document.querySelector(".comment_input");
 let send_button = document.querySelector(".send_button");
 let comment_description = document.querySelector(".comment_description");
 
-send_button.addEventListener("click", function () {
+send_button.addEventListener("click", function (e) {
+  e.preventDefault();
   let inputValue = comment_input.value;
 
   comment_description.innerHTML = inputValue;
