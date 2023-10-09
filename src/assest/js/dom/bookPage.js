8f9b3949d22a22b2dba714c4c99c0883628bcc0a
Ramal Abbassov <ramalabbasov044@gmail.com>
2023-10-09 17:32:04 +0400

    add

--- src/assest/js/dom/bookPage.js
+++ src/assest/js/dom/bookPage.js
@@ -37,6 +37,15 @@ send_button.addEventListener("click", function (e) {
   comment_input.value = "";
 });
 
+function getTimeDifferenceInDays(date) {
+    const currentDate = new Date();
+    const releaseDate = new Date(date);
+    const timeDifferenceInMilliseconds = currentDate - releaseDate;
+    const daysDifference = Math.floor(timeDifferenceInMilliseconds / (1000 * 60 * 60 * 24));
+    return daysDifference;
+  }
+  
+
 let book_url = window.location.hash
 let book_length = book_url.length
 let book_id = book_url.slice(4,book_length)
@@ -46,42 +55,46 @@ let renderBookPageDetails = () => {
         const bookData = snapshot.val();
         let bookDataToArr = Object.entries(bookData);
         let bookPageArr = bookDataToArr.filter((item) => item[0] == book_id)
-        let bookPage = bookPageArr.map((item) => `
-            <div class="book_top">
-                <div class="book_text">
-                    <a class="back_navigation" href="./catalog-page.html">
-                        <button class="back_btn">
-                            &lt; BACK </button>
-                    </a>
-                    <a href="">
-                        <button class="btn_primary">
-                            ${item[1].book_apperance.slice(0,4)}
-                        </button>
-                    </a>
-                    <p class="book_name">${item[1].book_name}</p>
-                    <p class="book_apperance">2 days ago added</p>
-                    <p class="book_writer">${item[1].book_author}</p>
-                    <p class="book_description">
-                        ${item[1].book_description}
-                    </p>
+        let bookPage = bookPageArr.map((item) => {
+            const daysAgo = getTimeDifferenceInDays(item[1].book_date);
 
-                    <form class="comment_form" action="">
-                        <input class="comment_input" type="text" placeholder="your anonim comment...">
-                        <button class="send_button">
-                            <img src="../assest/icons/bookpage/send.svg" alt=""></button>
-                    </form>
+            return  `
+                <div class="book_top">
+                    <div class="book_text">
+                        <a class="back_navigation" href="./catalog-page.html">
+                            <button class="back_btn">
+                                &lt; BACK </button>
+                        </a>
+                        <a href="">
+                            <button class="btn_primary">
+                                ${item[1].book_apperance.slice(0,4)}
+                            </button>
+                        </a>
+                        <p class="book_name">${item[1].book_name}</p>
+                        <p class="book_apperance">${daysAgo} Days Ago</p>
+                        <p class="book_writer">${item[1].book_author}</p>
+                        <p class="book_description">
+                            ${item[1].book_description}
+                        </p>
 
-                    <div class="comment_main">
-                        
+                        <form class="comment_form" action="">
+                            <input class="comment_input" type="text" placeholder="your anonim comment...">
+                            <button class="send_button">
+                                <img src="../assest/icons/bookpage/send.svg" alt=""></button>
+                        </form>
 
+                        <div class="comment_main">
+                            
+
+                        </div>
+                    </div>
+                    <div class="book_img">
+                    ${item[1].book_is_new ? `<div class="new_information_box">New</div>` : ``} 
+                        <img src="${item[1].book_img_url}" alt="">
                     </div>
                 </div>
-                <div class="book_img">
-                ${item[1].book_is_new ? `<div class="new_information_box">New</div>` : ``} 
-                    <img src="${item[1].book_img_url}" alt="">
-                </div>
-            </div>
-        `)
+            `
+        })
         book_page.innerHTML = bookPage
     });
 }
